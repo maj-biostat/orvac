cbb3d5336f3c59867183af072d699bc7f05387f4
Mark Jones <mark.jones@telethonkids.org.au>
2019-03-01 12:13:32 +0800

    Updated run script and reporting

--- code/simulations/sim_01/run_sim_2.sh
+++ code/simulations/sim_01/run_sim_2.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 # null case
-/usr/bin/Rscript main_4.R -f cfg1.yaml -o T -i "Null01" -l null_01.log -n 500 -s 9676 -a 50 -d 0.0 -b 0.3 -p 0.3 -m 30 -t 30
-/usr/bin/Rscript main_4.R -f cfg1.yaml -o T -i "Null02" -l null_02.log -n 500 -s 9676 -a 30 -d 0.0 -b 0.3 -p 0.3 -m 30 -t 30
-/usr/bin/Rscript main_4.R -f cfg1.yaml -o T -i "Null03" -l null_03.log -n 500 -s 9676 -a 50 -d 1.0 -b 0.3 -p 0.3 -m 30 -t 30
-/usr/bin/Rscript main_4.R -f cfg1.yaml -o T -i "Null04" -l null_04.log -n 500 -s 9676 -a 30 -d 1.0 -b 0.3 -p 0.3 -m 30 -t 30
+# /usr/bin/Rscript main_4.R -f cfg1.yaml -o T -i "Null01" -l null_01.log -n 500 -s 9676 -a 50 -d 0.0 -b 0.3 -p 0.3 -m 30 -t 30
+# /usr/bin/Rscript main_4.R -f cfg1.yaml -o T -i "Null02" -l null_02.log -n 500 -s 9676 -a 30 -d 0.0 -b 0.3 -p 0.3 -m 30 -t 30
+# /usr/bin/Rscript main_4.R -f cfg1.yaml -o T -i "Null03" -l null_03.log -n 500 -s 9676 -a 50 -d 1.0 -b 0.3 -p 0.3 -m 30 -t 30
+# /usr/bin/Rscript main_4.R -f cfg1.yaml -o T -i "Null04" -l null_04.log -n 500 -s 9676 -a 30 -d 1.0 -b 0.3 -p 0.3 -m 30 -t 30
+# 
+# /usr/bin/Rscript main_4.R -f cfg1.yaml -o T -i "I01" -l i_01.log       -n 500 -s 9676 -a 30 -d 1.0 -b 0.3 -p 0.4 -m 30 -t 30
+# /usr/bin/Rscript main_4.R -f cfg1.yaml -o T -i "I02" -l i_02.log       -n 500 -s 9676 -a 30 -d 1.0 -b 0.3 -p 0.5 -m 30 -t 30
+# /usr/bin/Rscript main_4.R -f cfg1.yaml -o T -i "I03" -l i_03.log       -n 500 -s 9676 -a 30 -d 1.0 -b 0.3 -p 0.6 -m 30 -t 30
+# 
+# /usr/bin/Rscript main_4.R -f cfg1.yaml -o T -i "IC01" -l i_c_01.log    -n 500 -s 9676 -a 30 -d 1.0 -b 0.3 -p 0.4 -m 30 -t 35
+# /usr/bin/Rscript main_4.R -f cfg1.yaml -o T -i "IC02" -l i_c_02.log    -n 500 -s 9676 -a 30 -d 1.0 -b 0.3 -p 0.4 -m 30 -t 40
+# /usr/bin/Rscript main_4.R -f cfg1.yaml -o T -i "IC03" -l i_c_03.log    -n 500 -s 9676 -a 30 -d 1.0 -b 0.3 -p 0.4 -m 30 -t 45
+# 
+# /usr/bin/Rscript main_4.R -f cfg1.yaml -o T -i "IC04" -l i_c_04.log    -n 500 -s 9676 -a 30 -d 1.0 -b 0.3 -p 0.5 -m 30 -t 35
+# /usr/bin/Rscript main_4.R -f cfg1.yaml -o T -i "IC05" -l i_c_05.log    -n 500 -s 9676 -a 30 -d 1.0 -b 0.3 -p 0.5 -m 30 -t 40
+# /usr/bin/Rscript main_4.R -f cfg1.yaml -o T -i "IC06" -l i_c_06.log    -n 500 -s 9676 -a 30 -d 1.0 -b 0.3 -p 0.5 -m 30 -t 45
 
-/usr/bin/Rscript main_4.R -f cfg1.yaml -o T -i "I01" -l i_01.log       -n 500 -s 9676 -a 30 -d 1.0 -b 0.3 -p 0.4 -m 30 -t 30
-/usr/bin/Rscript main_4.R -f cfg1.yaml -o T -i "I02" -l i_02.log       -n 500 -s 9676 -a 30 -d 1.0 -b 0.3 -p 0.5 -m 30 -t 30
-/usr/bin/Rscript main_4.R -f cfg1.yaml -o T -i "I03" -l i_03.log       -n 500 -s 9676 -a 30 -d 1.0 -b 0.3 -p 0.6 -m 30 -t 30
-
-/usr/bin/Rscript main_4.R -f cfg1.yaml -o T -i "IC01" -l i_c_01.log    -n 500 -s 9676 -a 30 -d 1.0 -b 0.3 -p 0.4 -m 30 -t 35
-/usr/bin/Rscript main_4.R -f cfg1.yaml -o T -i "IC02" -l i_c_02.log    -n 500 -s 9676 -a 30 -d 1.0 -b 0.3 -p 0.4 -m 30 -t 40
-/usr/bin/Rscript main_4.R -f cfg1.yaml -o T -i "IC03" -l i_c_03.log    -n 500 -s 9676 -a 30 -d 1.0 -b 0.3 -p 0.4 -m 30 -t 45
-
-/usr/bin/Rscript main_4.R -f cfg1.yaml -o T -i "IC04" -l i_c_04.log    -n 500 -s 9676 -a 30 -d 1.0 -b 0.3 -p 0.5 -m 30 -t 35
-/usr/bin/Rscript main_4.R -f cfg1.yaml -o T -i "IC05" -l i_c_05.log    -n 500 -s 9676 -a 30 -d 1.0 -b 0.3 -p 0.5 -m 30 -t 40
-/usr/bin/Rscript main_4.R -f cfg1.yaml -o T -i "IC06" -l i_c_06.log    -n 500 -s 9676 -a 30 -d 1.0 -b 0.3 -p 0.5 -m 30 -t 45
-
-/usr/bin/Rscript main_4.R -f cfg1.yaml -o T -i "IC07" -l i_c_07.log    -n 500 -s 9676 -a 30 -d 1.0 -b 0.3 -p 0.6 -m 30 -t 35
-/usr/bin/Rscript main_4.R -f cfg1.yaml -o T -i "IC08" -l i_c_08.log    -n 500 -s 9676 -a 30 -d 1.0 -b 0.3 -p 0.6 -m 30 -t 40
-/usr/bin/Rscript main_4.R -f cfg1.yaml -o T -i "IC09" -l i_c_09.log    -n 500 -s 9676 -a 30 -d 1.0 -b 0.3 -p 0.6 -m 30 -t 45
+#/usr/bin/Rscript main_4.R -f cfg1.yaml -o T -i "IC07" -l i_c_07.log    -n 500 -s 9676 -a 30 -d 1.0 -b 0.3 -p 0.6 -m 30 -t 35
+/usr/bin/Rscript main_4.R -f cfg1.yaml -o T -i "IC08" -l i_c_08.log    -n 500 -s 2513 -a 30 -d 1.0 -b 0.3 -p 0.6 -m 30 -t 40
+/usr/bin/Rscript main_4.R -f cfg1.yaml -o T -i "IC09" -l i_c_09.log    -n 500 -s 8411 -a 30 -d 1.0 -b 0.3 -p 0.6 -m 30 -t 45
 
 
 #/usr/bin/Rscript main_4.R -f cfg1.yaml -o T -i "Clin01" -l clin_01.log          -n 1000 -s 8676 -a 50 -d 0.0 -b 0.3 -p 0.3 -m 30 -t 35
