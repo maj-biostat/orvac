91e33b274bc55fe843ddee19bb92abd351c7d572
Mark Jones <mark.jones@telethonkids.org.au>
2019-04-02 19:40:54 +0800

    Simplifications, approximations

--- code/simulations/sim_01/sim_script_generator.R
+++ code/simulations/sim_01/sim_script_generator.R
@@ -2,11 +2,13 @@ library(dplyr)
 
 nsim <- 1000
 accrual <- c(30, 50)
-info_delay <- c(0)
+info_delay <- c(0,1)
 p0 <- c(0.1, 0.4, 0.7)
-pdelta <- c(0.05, 0.1, 0.15)
-t0 <- c(20, 30, 50)
-tdelta <- c(5, 10, 15)
+#pdelta <- c(0.05, 0.1, 0.15)
+pdelta <- c(0.0)
+t0 <- c(20, 35, 50)
+#tdelta <- c(5, 10, 15)
+tdelta <- c(0)
 
 d <- expand.grid(accrual, info_delay, 
                  p0, pdelta, 
