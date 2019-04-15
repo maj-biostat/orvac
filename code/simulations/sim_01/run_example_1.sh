359c0d95f847f51f8a7770b3837742cc2426506d
Mark Jones <mark.jones@telethonkids.org.au>
2019-04-15 20:31:00 +0800

    Removed redundant files

--- code/simulations/sim_01/run_example_1.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Script for running a couple of example trials for visualisation
-
-/usr/bin/Rscript run_from_script_test.R -f cfg1.yaml -o T -i "N04" -l i_01.log   -n 1000 -s 9441  -a 50 -d 1 -b 0.8 -p 0.80 -m 30 -t 30
